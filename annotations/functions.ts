3099e68c18104209f737b34df9818d2fc68be5fb
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-03-26 08:50:55 +0530

    ADD: Function annotations

---
+++ annotations/functions.ts
@@ -0,0 +1,11 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+}
