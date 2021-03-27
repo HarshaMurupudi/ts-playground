43efcfa7acd688e387c57db431cce80a2874e246
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-03-27 08:37:08 +0530

    ADD: Void and never return type functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -9,3 +9,25 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 }
+
+const logger = (message: string): void => {
+  console.log(message)
+}
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+}
+
+const throwErrorV2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return message
+}
+
+const throwErrorV3 = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+}
